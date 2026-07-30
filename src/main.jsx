ed6a0f3a42945e2087cefb19e7847ece5acc0bbe
carloosgo <carloos.go@gmail.com>
2026-07-30 16:00:32 -0600

    Carga ajustes finales de la cabecera flotante

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { I18nProvider } from './i18n/index.jsx';
 import './index.css';
+import './app/FloatingEditorPlacement.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <I18nProvider>
