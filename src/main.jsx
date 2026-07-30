3e683434d94517292324357babace0dbb81dac9f
carloosgo <carloos.go@gmail.com>
2026-07-30 09:13:40 -0600

    Registra PWA al iniciar la aplicación

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { I18nProvider } from './i18n/index.jsx';
+import { registerServiceWorker } from './pwa/registerServiceWorker.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,3 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </I18nProvider>
 );
+
+registerServiceWorker();
