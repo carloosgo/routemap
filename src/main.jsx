d6148943a04eacb676a34f5b177b9a1dac899403
carloosgo <carloos.go@gmail.com>
2026-07-30 17:30:20 -0600

    Carga ajustes visuales del módulo flotante

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import { I18nProvider } from './i18n/index.jsx';
 import './index.css';
 import './app/FloatingEditorPlacement.css';
+import './app/FloatingEditorPolish.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <I18nProvider>
