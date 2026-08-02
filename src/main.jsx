bb4b469e8efd346e679ed2b3691bc315e26b7000
carloosgo <carloos.go@gmail.com>
2026-08-01 20:10:13 -0600

    Remove legacy Mapbox landmark patch import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './modules/map/landmarkLayerPatch.js';
 import App from './App.jsx';
 import { I18nProvider } from './i18n/index.jsx';
 import './index.css';
