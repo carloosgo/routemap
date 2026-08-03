3535a3ae699b0ae6c3432bbb807598455d4f259d
carloosgo <carloos.go@gmail.com>
2026-08-03 12:41:12 -0600

    Enable outside dismissal for place save popup

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { I18nProvider } from './i18n/index.jsx';
+import './modules/map/placeSavePopupDismiss.js';
 import './index.css';
 import './app/FloatingEditorPlacement.css';
 import './app/FloatingEditorPolish.css';
