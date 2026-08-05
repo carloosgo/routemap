488c7f83be9f3a273d689d9dce8dadf80386fef3
carloosgo <carloos.go@gmail.com>
2026-08-05 15:23:41 -0600

    Carga ajustes visuales del menú y mapa

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import './app/FloatingEditorPlacement.css';
 import './app/FloatingEditorPolish.css';
 import './app/EditorNavigationIcons.css';
 import './app/SegmentInteractionColors.css';
+import './app/WorkspaceMenuMapPolish.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <I18nProvider>
