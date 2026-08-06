118b3cc4f8e858dc19088aa30fda6fc3067a9863
carloosgo <carloos.go@gmail.com>
2026-08-06 13:17:05 -0600

    Trigger i18n validation workflow

--- test/i18nBehavior.test.js
+++ test/i18nBehavior.test.js
@@ -1,3 +1,4 @@
+// Validation branch marker: exercises pull-request workflows against the completed i18n state.
 import test from 'node:test';
 import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
