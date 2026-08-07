4421c5ed8ef3191eb9bdea679bb9adf60452f7b1
carloosgo <carloos.go@gmail.com>
2026-08-07 15:30:40 -0600

    Update sparse settled dash tests

--- test/crispDashedRoutes.test.js
+++ test/crispDashedRoutes.test.js
@@ -9,25 +9,29 @@ async function read(relativePath) {
   return readFile(new URL(relativePath, root), 'utf8');
 }
 
-test('divide un trazo en guiones de 6px con espacios de 4px', () => {
+test('divide un trazo en guiones de 4px con espacios de 6px', () => {
   const segments = pixelDashSegments([
     { x: 0, y: 0 },
     { x: 30, y: 0 },
-  ], 6, 4);
+  ], 4, 6);
 
   assert.deepEqual(
     segments.map((segment) => [segment[0].x, segment.at(-1).x]),
-    [[0, 6], [10, 16], [20, 26]]
+    [[0, 4], [10, 14], [20, 24]]
   );
 });
 
-test('Itinerario usa segmentos Polyline nativos y no símbolos repetidos para el guionado', async () => {
+test('Itinerario usa segmentos Polyline nativos y espera a que el viewport quede estable', async () => {
   const googleMap = await read('src/modules/map/GooglePlacesMap.jsx');
   const renderer = await read('src/modules/map/crispDashedRoutes.js');
 
   assert.match(googleMap, /createCrispDashedRoutes\(/);
-  assert.match(googleMap, /dashPx: 6/);
-  assert.match(googleMap, /gapPx: 4/);
+  assert.match(renderer, /DEFAULT_DASH_PX = 4/);
+  assert.match(renderer, /DEFAULT_GAP_PX = 6/);
+  assert.match(renderer, /DEFAULT_STROKE_WEIGHT = 2/);
+  assert.match(renderer, /viewportSettled = false/);
+  assert.match(renderer, /map\.addListener\?\.\('idle'/);
+  assert.match(renderer, /requestAnimationFrame\(\(\) => \{/);
   assert.match(renderer, /new maps\.Polyline\(/);
   assert.match(renderer, /strokeOpacity: 1/);
   assert.doesNotMatch(renderer, /icons:|repeat:/);
