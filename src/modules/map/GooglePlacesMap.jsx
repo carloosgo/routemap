8d334214a175dc644bf61355d0948ac8fcf6c5c7
carloosgo <carloos.go@gmail.com>
2026-08-07 14:41:59 -0600

    Increase country fill slightly

--- src/modules/map/GooglePlacesMap.jsx
+++ src/modules/map/GooglePlacesMap.jsx
@@ -343,7 +343,7 @@ export function GooglePlacesMap({
             if (!color) return null;
             return {
               fillColor: color,
-              fillOpacity: 0.20,
+              fillOpacity: 0.21,
               strokeOpacity: 0,
               strokeWeight: 0,
             };
