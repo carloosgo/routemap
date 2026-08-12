4c305ecd94502c066764f11eab30d17a57814740
carloosgo <carloos.go@gmail.com>
2026-08-12 17:08:49 -0600

    refactor: isolate v4 pilot backend manifest

---
+++ functions/v4PilotBackendManifest.js
@@ -0,0 +1,10 @@
+export const V4_PILOT_BACKEND_REGION = 'us-central1';
+export const V4_PILOT_BACKEND_FUNCTION_NAMES = Object.freeze([
+  'v4SegmentAggregate',
+  'v4PlaceAggregate',
+  'v4ConnectionTouch',
+  'v4NoteTouch',
+  'v4ChecklistTouch',
+  'v4TripLifecycle',
+  'v4TripPurge',
+]);
