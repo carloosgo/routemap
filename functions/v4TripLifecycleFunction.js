4a888412bf4a85b8ae6c82ac6612f4a0df79d1a5
carloosgo <carloos.go@gmail.com>
2026-08-12 16:27:20 -0600

    refactor: expose v4 lifecycle function factory

--- functions/v4TripLifecycleFunction.js
+++ functions/v4TripLifecycleFunction.js
@@ -6,14 +6,34 @@ import {
   createV4TripLifecycleCallableHandler,
 } from './v4TripLifecycleCallableHandler.js';
 
-export const v4TripLifecycle = onCall(
-  callableOptions({
-    timeoutSeconds: 15,
-    maxInstances: 10,
-    concurrency: 20,
-  }),
-  createV4TripLifecycleCallableHandler({
-    db,
-    enforceRateLimit: (request) => enforceQuota(db, request, V4_TRIP_LIFECYCLE_QUOTA),
-  })
-);
+export function createV4TripLifecycleFunction({
+  adminDb,
+  callableFactory = onCall,
+  optionsFactory = callableOptions,
+  handlerFactory = createV4TripLifecycleCallableHandler,
+  quotaEnforcer = enforceQuota,
+} = {}) {
+  if (!adminDb) throw new TypeError('Se requiere Firestore Admin.');
+  if (typeof callableFactory !== 'function') throw new TypeError('callableFactory debe ser función.');
+  if (typeof optionsFactory !== 'function') throw new TypeError('optionsFactory debe ser función.');
+  if (typeof handlerFactory !== 'function') throw new TypeError('handlerFactory debe ser función.');
+  if (typeof quotaEnforcer !== 'function') throw new TypeError('quotaEnforcer debe ser función.');
+
+  return callableFactory(
+    optionsFactory({
+      timeoutSeconds: 15,
+      maxInstances: 10,
+      concurrency: 20,
+    }),
+    handlerFactory({
+      db: adminDb,
+      enforceRateLimit: (request) => quotaEnforcer(
+        adminDb,
+        request,
+        V4_TRIP_LIFECYCLE_QUOTA
+      ),
+    })
+  );
+}
+
+export const v4TripLifecycle = createV4TripLifecycleFunction({ adminDb: db });
