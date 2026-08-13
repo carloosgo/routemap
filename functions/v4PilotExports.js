7370abb6bf2b91be5a96976234fb039a2e9f1ec9
carloosgo <carloos.go@gmail.com>
2026-08-12 18:56:15 -0600

    Export Eventarc ingress instead of unsupported Firestore functions

--- functions/v4PilotExports.js
+++ functions/v4PilotExports.js
@@ -2,10 +2,6 @@ import { createV4PilotBackendBundle } from './v4PilotBackendBundle.js';
 
 const pilot = createV4PilotBackendBundle();
 
-export const v4SegmentAggregate = pilot.v4SegmentAggregate;
-export const v4PlaceAggregate = pilot.v4PlaceAggregate;
-export const v4ConnectionTouch = pilot.v4ConnectionTouch;
-export const v4NoteTouch = pilot.v4NoteTouch;
-export const v4ChecklistTouch = pilot.v4ChecklistTouch;
+export const v4FirestoreEventIngress = pilot.v4FirestoreEventIngress;
 export const v4TripLifecycle = pilot.v4TripLifecycle;
 export const v4TripPurge = pilot.v4TripPurge;
