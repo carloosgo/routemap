edb8e728451486258d0ef087b69f0a80a7f2bd5f
carloosgo <carloos.go@gmail.com>
2026-08-12 18:37:02 -0600

    Fix v4 pilot function regions for Firestore Eventarc

--- functions/v4PilotBackendManifest.js
+++ functions/v4PilotBackendManifest.js
@@ -1,4 +1,8 @@
-export const V4_PILOT_BACKEND_REGION = 'us-central1';
+export const V4_PILOT_FIRESTORE_EVENT_REGION = 'northamerica-south1';
+export const V4_PILOT_SERVICE_REGION = 'us-central1';
+// Compatibility alias for non-Firestore pilot services.
+export const V4_PILOT_BACKEND_REGION = V4_PILOT_SERVICE_REGION;
+
 export const V4_PILOT_BACKEND_FUNCTION_NAMES = Object.freeze([
   'v4SegmentAggregate',
   'v4PlaceAggregate',
@@ -8,3 +12,13 @@ export const V4_PILOT_BACKEND_FUNCTION_NAMES = Object.freeze([
   'v4TripLifecycle',
   'v4TripPurge',
 ]);
+
+export const V4_PILOT_BACKEND_FUNCTION_REGIONS = Object.freeze({
+  v4SegmentAggregate: V4_PILOT_FIRESTORE_EVENT_REGION,
+  v4PlaceAggregate: V4_PILOT_FIRESTORE_EVENT_REGION,
+  v4ConnectionTouch: V4_PILOT_FIRESTORE_EVENT_REGION,
+  v4NoteTouch: V4_PILOT_FIRESTORE_EVENT_REGION,
+  v4ChecklistTouch: V4_PILOT_FIRESTORE_EVENT_REGION,
+  v4TripLifecycle: V4_PILOT_SERVICE_REGION,
+  v4TripPurge: V4_PILOT_SERVICE_REGION,
+});
