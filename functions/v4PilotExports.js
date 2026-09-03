9631093f6bce3da3c486a7017f8ae10bb5c7ffc8
carloosgo <carloos.go@gmail.com>
2026-09-03 11:40:34 -0600

    Remove v4 migration and pilot backend scaffolding

--- functions/v4PilotExports.js
+++
@@ -1,7 +0,0 @@
-import { createV4PilotBackendBundle } from './v4PilotBackendBundle.js';
-
-const pilot = createV4PilotBackendBundle();
-
-export const v4FirestoreEventIngress = pilot.v4FirestoreEventIngress;
-export const v4TripLifecycle = pilot.v4TripLifecycle;
-export const v4TripPurge = pilot.v4TripPurge;
