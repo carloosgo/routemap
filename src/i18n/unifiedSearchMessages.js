94e211bdfbb2e4ef828f039b1806fe8712fb7068
carloosgo <carloos.go@gmail.com>
2026-09-08 15:26:43 -0600

    i18n: add unified city and place search messages

---
+++ src/i18n/unifiedSearchMessages.js
@@ -0,0 +1,18 @@
+export const unifiedSearchMessages = Object.freeze({
+  es: Object.freeze({
+    addCity: 'Agregar ciudad',
+    cityAlreadyAdded: 'Ciudad agregada',
+    moveCity: 'Mover ciudad',
+    removeCity: 'Eliminar ciudad',
+    placeWaitingForDates: 'Este lugar ya pertenece a esta ciudad. Agrega fechas para asignarle su día del viaje.',
+    placeCityResolveError: 'No fue posible identificar la ciudad de este lugar.',
+  }),
+  en: Object.freeze({
+    addCity: 'Add city',
+    cityAlreadyAdded: 'City added',
+    moveCity: 'Move city',
+    removeCity: 'Remove city',
+    placeWaitingForDates: 'This place already belongs to this city. Add dates to assign its trip day.',
+    placeCityResolveError: 'The city for this place could not be identified.',
+  }),
+});
