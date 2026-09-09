5152310c632e3e78bbb95d08bc75b26148233cc3
carloosgo <carloos.go@gmail.com>
2026-09-09 15:47:16 -0600

    fix: remove redundant segment date inputs from expenses

--- src/modules/trips/SegmentBody.jsx
+++ src/modules/trips/SegmentBody.jsx
@@ -1,5 +1,3 @@
-import { CalendarDateInput } from '../../components/CalendarDateInput.jsx';
-import { useTranslation } from '../../i18n/index.jsx';
 import { ExpenseEditor } from '../expenses/ExpenseEditor.jsx';
 
 export function SegmentBody({
@@ -7,42 +5,10 @@ export function SegmentBody({
   currency,
   locale,
   bodyId,
-  dateError = '',
-  calendarReferenceDate = '',
-  onUpdate,
   onUpdateExpenses,
 }) {
-  const { t } = useTranslation();
-
   return (
     <div className="segment__body segment-expense-form" id={bodyId}>
-      <div className="dates">
-        <div className="dates__row">
-          <CalendarDateInput
-            value={segment.startDate}
-            max={segment.endDate || undefined}
-            referenceDate={calendarReferenceDate || undefined}
-            locale={locale}
-            ariaLabel={t('startDate')}
-            onChange={(startDate) => onUpdate({ startDate })}
-          />
-          <CalendarDateInput
-            value={segment.endDate}
-            min={segment.startDate || undefined}
-            referenceDate={calendarReferenceDate || undefined}
-            locale={locale}
-            ariaLabel={t('endDate')}
-            align="end"
-            onChange={(endDate) => onUpdate({ endDate })}
-          />
-        </div>
-        {dateError && (
-          <p className="segment-details-modal__date-error" role="alert">
-            {dateError}
-          </p>
-        )}
-      </div>
-
       <ExpenseEditor
         expenses={segment.expenses}
         currency={currency}
