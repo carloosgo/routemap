5c0d09fa11a66602590241529aefdb3d564f1127
carloosgo <carloos.go@gmail.com>
2026-09-15 00:25:45 -0600

    feat: route public share URLs to read-only view

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import SharedItineraryPage from './app/SharedItineraryPage.jsx';
 import { I18nProvider } from './i18n/index.jsx';
 import './modules/map/placeSavePopupDismiss.js';
 import './index.css';
@@ -20,8 +21,13 @@ import './app/NotePanelPlacement.css';
 import './app/FloatingItineraryPanel.css';
 import './modules/trips/ItineraryCompactTen.css';
 
+const sharedRoute = globalThis.location?.pathname?.match(/^\/share\/([^/]+)\/?$/);
+const content = sharedRoute
+  ? <SharedItineraryPage shareId={decodeURIComponent(sharedRoute[1])} />
+  : <App />;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <I18nProvider>
-    <App />
+    {content}
   </I18nProvider>
 );
