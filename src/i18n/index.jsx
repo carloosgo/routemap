c59e494b71d1cb58d768a1fee208af3ac62e418c
carloosgo <carloos.go@gmail.com>
2026-09-15 00:38:32 -0600

    fix: use core dictionaries for share translations

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -9,12 +9,11 @@ import {
 import { config } from '../config.js';
 import es from './es.js';
 import en from './en.js';
-import { itineraryShareMessages } from './itineraryShare.js';
 import { storageV4Messages } from './storageV4.js';
 
 const dictionaries = {
-  es: { ...es, ...storageV4Messages.es, ...itineraryShareMessages.es },
-  en: { ...en, ...storageV4Messages.en, ...itineraryShareMessages.en },
+  es: { ...es, ...storageV4Messages.es },
+  en: { ...en, ...storageV4Messages.en },
 };
 const LOCALE_STORAGE_KEY = 'atlas:locale';
 const localeMetadata = Object.freeze({
